844f6d9b95648413d21abc502a55a69c0198cdb9
Crowdbotics <engineering@crowdbotics.com>
2023-02-24 00:29:05 +0000

    Update store.

---
+++ store/countryTest/api.js
@@ -0,0 +1,9 @@
+import axios from "axios"
+const countryTest = axios.create({
+  baseURL: "https://restcountries.com/v2/name/{name}",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+function countrytest_get_name_list(payload) {
+  return countryTest.get(`/name`)
+}
+export const apiService = { countrytest_get_name_list }
---
+++ store/countryTest/countrytest_response_get_countries.slice.js
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { apiService } from "./api"
+export const countrytest_get_name_list = createAsyncThunk(
+  "countrytest_response_get_countries/countrytest_get_name_list",
+  async payload => {
+    const response = await apiService.countrytest_get_name_list(payload)
+    return response.data
+  }
+)
+const initialState = { entities: [], api: { loading: "idle", error: null } }
+const countrytest_response_get_countriesSlice = createSlice({
+  name: "countrytest_response_get_countries",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [countrytest_get_name_list.pending]: (state, action) => {
+      if (state.api.loading === "idle") {
+        state.api.loading = "pending"
+      }
+    },
+    [countrytest_get_name_list.fulfilled]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.entities = action.payload
+        state.api.loading = "idle"
+      }
+    },
+    [countrytest_get_name_list.rejected]: (state, action) => {
+      if (state.api.loading === "pending") {
+        state.api.error = action.error
+        state.api.loading = "idle"
+      }
+    }
+  }
+})
+export default {
+  countrytest_get_name_list,
+  slice: countrytest_response_get_countriesSlice
+}
